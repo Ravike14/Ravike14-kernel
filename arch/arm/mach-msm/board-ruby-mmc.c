3ab9902c08da4fffcbaeeb3720339536b4bc3249
Ravishka <ravishka@ravishka-G41D3C.(none)>
2014-05-05 13:21:58 +0530

    tiwlan fix according to ruby

--- arch/arm/mach-msm/board-ruby-mmc.c
+++ arch/arm/mach-msm/board-ruby-mmc.c
@@ -1,4 +1,6 @@
-/* Copyright (C) 2008 HTC Corporation.
+/* linux/arch/arm/mach-msm/board-ruby-mmc.c
+ *
+ * Copyright (C) 2008 HTC Corporation.
  *
  * This software is licensed under the terms of the GNU General Public
  * License version 2, as published by the Free Software Foundation, and
@@ -9,7 +11,9 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  */
+
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
 #include <linux/delay.h>
@@ -18,7 +22,6 @@
 #include <linux/err.h>
 #include <linux/debugfs.h>
 #include <linux/gpio.h>
-#include <linux/module.h>
 
 #include <asm/gpio.h>
 #include <asm/io.h>
@@ -27,29 +30,15 @@
 #include <mach/htc_pwrsink.h>
 
 #include <asm/mach/mmc.h>
-#include <mach/msm_iomap.h>
-#include <linux/mfd/pmic8058.h>
-#include <mach/htc_sleep_clk.h>
 
 #include "devices.h"
 #include "board-ruby.h"
-#include <mach/proc_comm.h>
 #include "board-ruby-mmc.h"
-#include <mach/mpm.h>
-
-#include <linux/irq.h>
-
-#include <mach/rpm.h>
-#include <mach/rpm-regulator.h>
-
-#include "rpm_resources.h"
-
-int msm_proc_comm(unsigned cmd, unsigned *data1, unsigned *data2);
-
-#define PM8058_GPIO_BASE			NR_MSM_GPIOS
-#define PM8058_GPIO_PM_TO_SYS(pm_gpio)		(pm_gpio + PM8058_GPIO_BASE)
-#define PM8058_GPIO_SYS_TO_PM(sys_gpio)		(sys_gpio - PM8058_GPIO_BASE)
-
+#include "proc_comm.h"
+#include <mach/msm_iomap.h>
+#include <linux/mfd/pmic8058.h>
+#include <mach/htc_sleep_clk.h>
+#include <mach/htc_fast_clk.h>
 
 #if 0
 static int msm_sdcc_cfg_mpm_sdiowakeup(struct device *dev, unsigned mode)
@@ -60,7 +49,6 @@ static int msm_sdcc_cfg_mpm_sdiowakeup(struct device *dev, unsigned mode)
 
 	pdev = container_of(dev, struct platform_device, dev);
 
-	/* Only SDCC4 slot connected to WLAN chip has wakeup capability */
 	if (pdev->id == 4)
 		pin = MSM_MPM_PIN_SDC4_DAT1;
 	else
@@ -88,21 +76,15 @@ static int msm_sdcc_cfg_mpm_sdiowakeup(struct device *dev, unsigned mode)
 }
 #endif
 
-extern int msm_add_sdcc(unsigned int controller, struct mmc_platform_data *plat);
-
-/* ---- SDCARD ---- */
-/* ---- WIFI ---- */
-
+int msm_proc_comm(unsigned cmd, unsigned *data1, unsigned *data2);
 
 static uint32_t wifi_on_gpio_table[] = {
-    GPIO_CFG(RUBY_GPIO_WIFI_IRQ, 0, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_4MA), 
+	GPIO_CFG(RUBY_GPIO_WIFI_IRQ, 0, GPIO_CFG_INPUT, GPIO_CFG_NO_PULL, GPIO_CFG_4MA),
 };
 
 static uint32_t wifi_off_gpio_table[] = {
 	GPIO_CFG(RUBY_GPIO_WIFI_IRQ, 0, GPIO_CFG_INPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_4MA),
 };
-
-
 
 static void config_gpio_table(uint32_t *table, int len)
 {
@@ -117,15 +99,35 @@ static void config_gpio_table(uint32_t *table, int len)
 	}
 }
 
+#ifdef CONFIG_TIWLAN_SDIO
+static struct sdio_embedded_func wifi_func_array[] = {
+	{
+		.f_class        = SDIO_CLASS_NONE,
+		.f_maxblksize   = 512,
+	},
+	{
+		.f_class        = SDIO_CLASS_WLAN,
+		.f_maxblksize   = 512,
+	},
+};
+
 static struct embedded_sdio_data ruby_wifi_emb_data = {
-	.cccr	= {
-		.sdio_vsn	= 2,
+	.cis    = {
+		.vendor         = SDIO_VENDOR_ID_TI,
+		.device         = SDIO_DEVICE_ID_TI_WL12xx,
+		.blksize        = 512,
+		.max_dtr        = 25000000,
+	},
+	.cccr   = {
 		.multi_block	= 1,
 		.low_speed	= 0,
-		.wide_bus	= 0,
-		.high_power	= 1,
-		.high_speed	= 1,
-	}
+		.wide_bus	= 1,
+		.high_power	= 0,
+		.high_speed	= 0,
+		.disable_cd	= 1,
+	},
+	.funcs  = wifi_func_array,
+	.num_funcs = 2,
 };
 
 static void (*wifi_status_cb)(int card_present, void *dev_id);
@@ -149,21 +151,28 @@ static unsigned int ruby_wifi_status(struct device *dev)
 {
 	return ruby_wifi_cd;
 }
+#endif
 
-static unsigned int ruby_wifislot_type = MMC_TYPE_SDIO_WIFI;
+static unsigned int ruby_sdc_slot_type = MMC_TYPE_SDIO_WIFI;
 static struct mmc_platform_data ruby_wifi_data = {
-	.ocr_mask               = MMC_VDD_28_29,
-	.status                 = ruby_wifi_status,
-	.register_status_notify = ruby_wifi_status_register,
-	.embedded_sdio          = &ruby_wifi_emb_data,
-	.mmc_bus_width  = MMC_CAP_4_BIT_DATA,
-	.slot_type = &ruby_wifislot_type,
-	.msmsdcc_fmin   = 400000,
-	.msmsdcc_fmid   = 24000000,
-	.msmsdcc_fmax   = 48000000,
-	.nonremovable   = 0,
+	.ocr_mask		= MMC_VDD_165_195, 
+//#else
+//	.ocr_mask		= MMC_VDD_28_29, 
+//#endif
+#ifdef CONFIG_TIWLAN_SDIO
+	.status			= ruby_wifi_status,
+	.register_status_notify	= ruby_wifi_status_register,
+	.embedded_sdio		= &ruby_wifi_emb_data,
+#endif
+	.mmc_bus_width		= MMC_CAP_4_BIT_DATA,
+	.msmsdcc_fmin		= 400000,
+	.msmsdcc_fmid		= 24000000,
+	.msmsdcc_fmax		= 48000000,
+	.slot_type		= &ruby_sdc_slot_type,
+	.nonremovable		= 1,
 };
 
+#ifdef CONFIG_TIWLAN_SDIO
 int ruby_wifi_set_carddetect(int val)
 {
 	printk(KERN_INFO "%s: %d\n", __func__, val);
@@ -175,30 +184,47 @@ int ruby_wifi_set_carddetect(int val)
 	return 0;
 }
 EXPORT_SYMBOL(ruby_wifi_set_carddetect);
+#endif
 
-int ruby_wifi_power(int on)
+int ti_wifi_power(int on)
 {
+
 	const unsigned SDC4_HDRV_PULL_CTL_ADDR = (unsigned) MSM_TLMM_BASE + 0x20A0;
 
 	printk(KERN_INFO "%s: %d\n", __func__, on);
 
 	if (on) {
-		writel(0x1FDB, SDC4_HDRV_PULL_CTL_ADDR);
+		htc_wifi_bt_fast_clk_ctl(on, ID_WIFI);
+		mdelay(100);
+		htc_wifi_bt_sleep_clk_ctl(on, ID_WIFI);
+		mdelay(100);
+		gpio_set_value(RUBY_GPIO_WIFI_SHUTDOWN_N, 1); 
+		msleep(15);
+		gpio_set_value(RUBY_GPIO_WIFI_SHUTDOWN_N, 0);
+		msleep(1);
+		gpio_set_value(RUBY_GPIO_WIFI_SHUTDOWN_N, 1);
+		msleep(70);
 		config_gpio_table(wifi_on_gpio_table,
 				  ARRAY_SIZE(wifi_on_gpio_table));
+		mdelay(200);
+		writel(0x1FDB, SDC4_HDRV_PULL_CTL_ADDR);
 	} else {
-		writel(0x0BDB, SDC4_HDRV_PULL_CTL_ADDR);
+		writel(0x1FDB, SDC4_HDRV_PULL_CTL_ADDR);
 		config_gpio_table(wifi_off_gpio_table,
 				  ARRAY_SIZE(wifi_off_gpio_table));
+		gpio_set_value(RUBY_GPIO_WIFI_SHUTDOWN_N, on);
+		mdelay(1);
+		htc_wifi_bt_sleep_clk_ctl(on, ID_WIFI);
+		mdelay(1);
+		htc_wifi_bt_fast_clk_ctl(on, ID_WIFI);
 	}
-	
-	mdelay(1);
-	gpio_set_value(RUBY_GPIO_WIFI_SHUTDOWN_N, on); 
+
 
 	mdelay(120);
+
 	return 0;
 }
-EXPORT_SYMBOL(ruby_wifi_power);
+EXPORT_SYMBOL(ti_wifi_power);
 
 
 static int ruby_wifi_reset_state;
@@ -212,13 +238,18 @@ int ruby_wifi_reset(int on)
 int __init ruby_init_mmc()
 {
 	uint32_t id;
+	const unsigned SDC4_HDRV_PULL_CTL_ADDR = (unsigned)(MSM_TLMM_BASE + 0x20A0);
+#ifdef CONFIG_TIWLAN_SDIO
 	wifi_status_cb = NULL;
-
+#endif
 
 	printk(KERN_INFO "ruby: %s\n", __func__);
 
 	id = GPIO_CFG(RUBY_GPIO_WIFI_SHUTDOWN_N, 0, GPIO_CFG_OUTPUT,
 		GPIO_CFG_NO_PULL, GPIO_CFG_2MA);
+
+	writel(0x1FDB, SDC4_HDRV_PULL_CTL_ADDR);
+
 	gpio_tlmm_config(id, 0);
 	gpio_set_value(RUBY_GPIO_WIFI_SHUTDOWN_N, 0);
 
--- arch/arm/mach-msm/board-ruby-wifi.c
+++ arch/arm/mach-msm/board-ruby-wifi.c
@@ -1,3 +1,5 @@
+/* linux/arch/arm/mach-msm/board-ruby-wifi.c
+*/
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/platform_device.h>
@@ -10,12 +12,18 @@
 #include <linux/wifi_tiwlan.h>
 
 #include "board-ruby.h"
-#include "board-ruby-wifi.h"
 
-int ruby_wifi_power(int on);
-int ruby_wifi_reset(int on);
-int ruby_wifi_set_carddetect(int on);
-int ruby_wifi_get_mac_addr(unsigned char *buf);
+#include <linux/wl12xx.h>
+
+static struct wl12xx_platform_data ruby_wlan_data __initdata = {
+       .irq = MSM_GPIO_TO_INT(RUBY_GPIO_WIFI_IRQ),
+       .board_ref_clock = WL12XX_REFCLOCK_26,
+       .board_tcxo_clock = 1,
+};
+
+extern int ti_wifi_power(int on);
+extern int ruby_wifi_reset(int on);
+extern int ruby_wifi_get_mac_addr(unsigned char *buf);
 
 #define PREALLOC_WLAN_NUMBER_OF_SECTIONS	4
 #define PREALLOC_WLAN_NUMBER_OF_BUFFERS		160
@@ -27,7 +35,6 @@ int ruby_wifi_get_mac_addr(unsigned char *buf);
 #define WLAN_SECTION_SIZE_3	(PREALLOC_WLAN_NUMBER_OF_BUFFERS * 1024)
 
 #define WLAN_SKB_BUF_NUM	16
-
 
 static struct sk_buff *wlan_static_skb[WLAN_SKB_BUF_NUM];
 
@@ -75,27 +82,24 @@ int __init ruby_init_wifi_mem(void)
 
 static struct resource ruby_wifi_resources[] = {
 	[0] = {
-		.name		= "bcmdhd_wlan_irq",
+		.name		= "device_wifi_irq",
 		.start		= MSM_GPIO_TO_INT(RUBY_GPIO_WIFI_IRQ),
 		.end		= MSM_GPIO_TO_INT(RUBY_GPIO_WIFI_IRQ),
-#ifdef HW_OOB
-		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHLEVEL | IORESOURCE_IRQ_SHAREABLE,
-#else
-		.flags          = IORESOURCE_IRQ | IORESOURCE_IRQ_HIGHEDGE,
-#endif
+		.flags      = IORESOURCE_IRQ | IORESOURCE_IRQ_LOWEDGE,
 	},
 };
 
 static struct wifi_platform_data ruby_wifi_control = {
-	.set_power      = ruby_wifi_power,
+	.set_power      = ti_wifi_power,
 	.set_reset      = ruby_wifi_reset,
+#ifdef CONFIG_TIWLAN_SDIO
 	.set_carddetect = ruby_wifi_set_carddetect,
+#endif
 	.mem_prealloc   = ruby_wifi_mem_prealloc,
-	.get_mac_addr	= ruby_wifi_get_mac_addr,
 };
 
 static struct platform_device ruby_wifi_device = {
-	.name           = "bcmdhd_wlan",
+	.name           = "device_wifi",
 	.id             = 1,
 	.num_resources  = ARRAY_SIZE(ruby_wifi_resources),
 	.resource       = ruby_wifi_resources,
@@ -104,168 +108,17 @@ static struct platform_device ruby_wifi_device = {
 	},
 };
 
-static unsigned ruby_wifi_update_nvs(char *str)
-{
-#define NVS_LEN_OFFSET		0x0C
-#define NVS_DATA_OFFSET		0x40
-	unsigned char *ptr;
-	unsigned len;
-
-	if (!str)
-		return -EINVAL;
-	ptr = get_wifi_nvs_ram();
-	
-	memcpy(&len, ptr + NVS_LEN_OFFSET, sizeof(len));
-
-	
-	if (ptr[NVS_DATA_OFFSET + len - 1] == 0)
-		len -= 1;
-
-	if (ptr[NVS_DATA_OFFSET + len - 1] != '\n') {
-		len += 1;
-		ptr[NVS_DATA_OFFSET + len - 1] = '\n';
-	}
-
-	strcpy(ptr + NVS_DATA_OFFSET + len, str);
-	len += strlen(str);
-	memcpy(ptr + NVS_LEN_OFFSET, &len, sizeof(len));
-	return 0;
-}
-
-#ifdef HW_OOB
-static unsigned strip_nvs_param(char *param)
-{
-	unsigned char *nvs_data;
-
-	unsigned param_len;
-	int start_idx, end_idx;
-
-	unsigned char *ptr;
-	unsigned len;
-
-	if (!param)
-		return -EINVAL;
-	ptr = get_wifi_nvs_ram();
-	
-	memcpy(&len, ptr + NVS_LEN_OFFSET, sizeof(len));
-
-	
-	if (ptr[NVS_DATA_OFFSET + len - 1] == 0)
-		len -= 1;
-
-	nvs_data = ptr + NVS_DATA_OFFSET;
-
-	param_len = strlen(param);
-
-	
-	for (start_idx = 0; start_idx < len - param_len; start_idx++) {
-		if (memcmp(&nvs_data[start_idx], param, param_len) == 0)
-			break;
-	}
-
-	end_idx = 0;
-	if (start_idx < len - param_len) {
-		
-		for (end_idx = start_idx + param_len; end_idx < len; end_idx++) {
-			if (nvs_data[end_idx] == '\n' || nvs_data[end_idx] == 0)
-				break;
-		}
-	}
-
-	if (start_idx < end_idx) {
-		
-		for (; end_idx + 1 < len; start_idx++, end_idx++)
-			nvs_data[start_idx] = nvs_data[end_idx+1];
-
-		len = len - (end_idx - start_idx + 1);
-		memcpy(ptr + NVS_LEN_OFFSET, &len, sizeof(len));
-	}
-	return 0;
-}
-#endif
-
-#define WIFI_MAC_PARAM_STR     "macaddr="
-#define WIFI_MAX_MAC_LEN       17 
-
-static uint
-get_mac_from_wifi_nvs_ram(char *buf, unsigned int buf_len)
-{
-	uint len = 0;
-	unsigned char *nvs_ptr;
-	unsigned char *mac_ptr;
-
-	if (!buf || !buf_len)
-		return 0;
-	nvs_ptr = get_wifi_nvs_ram();
-	if (nvs_ptr)
-		nvs_ptr += NVS_DATA_OFFSET;
-
-	mac_ptr = strstr(nvs_ptr, WIFI_MAC_PARAM_STR);
-	if (mac_ptr) {
-		mac_ptr += strlen(WIFI_MAC_PARAM_STR);
-
-		
-		while (mac_ptr[0] == ' ')
-			mac_ptr++;
-
-		
-		len = 0;
-		while (mac_ptr[len] != '\r' && mac_ptr[len] != '\n' &&
-			mac_ptr[len] != '\0') {
-			len++;
-		}
-
-		if (len > buf_len)
-			len = buf_len;
-
-		memcpy(buf, mac_ptr, len);
-	}
-	return len;
-}
-
-#define ETHER_ADDR_LEN 6
-int ruby_wifi_get_mac_addr(unsigned char *buf)
-{
-	static u8 ether_mac_addr[] = {0x00, 0x11, 0x22, 0x33, 0x44, 0xFF};
-	char mac[WIFI_MAX_MAC_LEN];
-	unsigned mac_len;
-	unsigned int macpattern[ETHER_ADDR_LEN];
-	int i;
-
-	mac_len = get_mac_from_wifi_nvs_ram(mac, WIFI_MAX_MAC_LEN);
-	if (mac_len > 0) {
-		
-		sscanf(mac, "%02x:%02x:%02x:%02x:%02x:%02x",
-		&macpattern[0], &macpattern[1], &macpattern[2],
-		&macpattern[3], &macpattern[4], &macpattern[5]
-		);
-
-		for (i = 0; i < ETHER_ADDR_LEN; i++)
-			ether_mac_addr[i] = (u8)macpattern[i];
-	}
-
-	memcpy(buf, ether_mac_addr, sizeof(ether_mac_addr));
-
-	printk(KERN_INFO"ruby_wifi_get_mac_addr = %02x %02x %02x %02x %02x %02x \n",
-		ether_mac_addr[0], ether_mac_addr[1], ether_mac_addr[2], ether_mac_addr[3], ether_mac_addr[4], ether_mac_addr[5]);
-
-	return 0;
-}
-
 int __init ruby_wifi_init(void)
 {
 	int ret;
 
 	printk(KERN_INFO "%s: start\n", __func__);
-#ifdef HW_OOB
-	strip_nvs_param("sd_oobonly");
-#else
-	ruby_wifi_update_nvs("sd_oobonly=1\n");
-#endif
-	ruby_wifi_update_nvs("btc_params80=0\n");
-	ruby_wifi_update_nvs("btc_params6=30\n");
 	ruby_init_wifi_mem();
 	ret = platform_device_register(&ruby_wifi_device);
+
+       if(wl12xx_set_platform_data(&ruby_wlan_data))
+               pr_err("Error setting wl12xx_data\n");
+
 	return ret;
 }
 
